648e84572e1d1e857ffda0ba3b506797e5e37c7a
Aserrano7 <serranoandres022@gmail.com>
2023-12-23 23:32:10 -0800

    added route for root and /notes

--- server.js
+++ server.js
@@ -8,3 +8,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
+});
+
