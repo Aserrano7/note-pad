566099c00ef0e2af2a14bf4f765bf3ac2c7c28c2
Aserrano7 <serranoandres022@gmail.com>
2023-12-23 23:50:12 -0800

    added a route to handle /api/noted endpoints and return json

--- server.js
+++ server.js
@@ -12,8 +12,16 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
+
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"))
+});
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (err, data) => {
+    if (err) return console.log(err);
+    res.json(JSON.parse(data));
+  });
 });
 
 app.listen(PORT, () => {
