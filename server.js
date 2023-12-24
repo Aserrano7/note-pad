380f3173d7159d37fafcb651d83ed62f23f48304
Aserrano7 <serranoandres022@gmail.com>
2023-12-23 23:28:43 -0800

    added middleware and URL -encoded data as wel as converting it to json

--- server.js
+++ server.js
@@ -5,3 +5,6 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
